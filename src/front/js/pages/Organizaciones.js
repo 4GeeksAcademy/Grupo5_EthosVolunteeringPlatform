79e17d496f917fd1e9effee7ff4e28d49e2f7a50
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-09-11 22:54:14 +0000

    [Added organizations cards]

--- src/front/js/pages/Organizaciones.js
+++ src/front/js/pages/Organizaciones.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
 import { PartnersBanner } from '../component/partnersBanner';
 import { AuxiliarWhiteFive } from '../component/auxiliarWhiteFive';
-import OrganizacionesCards from '../component/OrganizacionesCards';
+import { CardsOrgs } from '../component/cardsOrgs';
+import {DataOrganizations} from '../component/DataOrganizations';
 import { AuxiliarGreenTwo} from '../component/auxiliarGreenTwo';
 import { Sponsors } from '../component/sponsors';
 import { FatFooter } from "../component/fatFooter";
 
 
 export const Organizaciones = () => {
+
+  const [item, setItem] = useState(DataOrganizations); //pupulate cards
+
+
   return (
     <>
       <div className='mt-4 mb-5 text-center'>
@@ -19,8 +25,8 @@ export const Organizaciones = () => {
         </div>
 
 
-        <div className='mt-4 mb-5'>
-          <OrganizacionesCards />
+        <div className='mt-4 mb-5 pb-4'>
+          <CardsOrgs item={item} />
         </div>
         
         <AuxiliarGreenTwo/>
