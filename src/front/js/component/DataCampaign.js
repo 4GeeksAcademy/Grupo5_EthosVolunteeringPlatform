35ce733fe1a9b0e21d2383c351120fe13195f956
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-09-11 20:51:08 +0000

    [Added campaign cards component]

---
+++ src/front/js/component/DataCampaign.js
@@ -0,0 +1,151 @@
+
+
+    const DataCampaign = [
+        {
+            id: 1,
+            title: "Orotina Verde",
+            category: "Ambiente",
+            img: "https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1913&q=80",
+            description: "Empieza la decimoquinta jornada de arborización urbana en los cinco distritos del cantón de Orotina.",
+            organization : "Municipalidad de Orotina",
+            date: "Septiembre 23",
+            duration: "1 día"
+    
+        },
+    
+        {
+            id: 2,
+            title: "STEMinistas!",
+            category: "Cultura",
+            img: "https://images.unsplash.com/photo-1653566031536-4d1b6a9da15e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+            description: "Convocatoria de científicas y programadoras para talleres con niñas incursionando en STEM.",
+            organization : "500 Científicas",
+            date: "Octubre 1",
+            duration: "3 días"
+    
+        },
+    
+        {
+            id: 3,
+            title: "Upala Saludable",
+            category: "Salud y bienestar",
+            img: "https://images.unsplash.com/photo-1590105577767-e21a1067899f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1856&q=80",
+            description: "Convocatoria de profesionales de salud para jornadas de valoración en el cantón de Upala.",
+            organization : "OneHealth",
+            date: "Octubre 7",
+            duration: "5 días"
+        },
+    
+        {
+            id: 4,
+            title: "Surfing 4Reefs",
+            category: "Ambiente",
+            img: "https://images.unsplash.com/photo-1598442697644-586a4704a82a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+            description: "Inscríbete al Heat bénefico y apoya la restauración de arrecifes de GrowingReefs en Cahuita.",
+            organization : "GrowingReefs",
+            date: "Octubre 22",
+            duration: "2 días"
+    
+        },
+    
+        {
+            id: 5,
+            title: "Caribbean Palettes",
+            category: "Cultura",
+            img: "https://images.unsplash.com/photo-1613168461287-6e529b708c76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+            description: "Participa en la elaboración del nuevo conjunto de murales y mosaicos conceptuales en Limón centro.",
+            organization : "Casa Cultura Limón",
+            date: "Octubre 17",
+            duration: "1 día"
+    
+        },
+    
+        {
+            id: 6,
+            title: "Donación de Sangre",
+            category: "Salud y bienestar",
+            img: "https://images.unsplash.com/photo-1536856136534-bb679c52a9aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+            description: "La sangre es un precioso presente. Apoya al Banco Nacional de Sangre en esta jornada de donación.",
+            organization : "Banco Nacional de Sangre",
+            date: "Octubre 15",
+            duration: "7 días"
+        },
+    
+        {
+            id: 7,
+            title: "San José Fluye",
+            category: "Ambiente",
+            img: "https://images.unsplash.com/photo-1652971799012-41787043b35d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2039&q=80",
+            description: "Apúntate a las jornadas de limpieza de la cuenca del Río Torres en los barrios Otoya y Amón.",
+            organization : "MCJ",
+            date: "Octubre 28",
+            duration: "1 día"
+    
+        },
+    
+        {
+            id: 8,
+            title: "LESCOnciertos",
+            category: "Cultura",
+            img: "https://images.unsplash.com/photo-1547637886-84deffdf4c17?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+            description: "Convocatoria de músicos e interprétes de LESCO para jornada de conciertos inclusivos.",
+            organization : "MCJ",
+            date: "Septiembre 29",
+            duration: "3 días"
+    
+        },
+    
+        {
+            id: 9,
+            title: "JND Adaptados",
+            category: "Salud y bienestar",
+            img: "https://images.unsplash.com/photo-1526676037777-05a232554f77?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+            description: "Participa como voluntario durante los Juegos Deportivos Nacionales Adaptados. Apoya a los atletas desde la logística.",
+            organization : "JND Adaptados",
+            date: "Octubre 31",
+            duration: "5 días"
+        },
+
+        {
+            id: 10,
+            title: "Librería Libre",
+            category: "Cultura",
+            img: "https://images.unsplash.com/photo-1547637886-84deffdf4c17?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+            description: "La Librería Libre hará su nuevo ciclo en La Sabana. Para retirar un nuevo ¡simplemente donas uno que ya hayas usado!",
+            organization : "MCJ",
+            date: "Octubre 31",
+            duration: "5 días"
+    
+        },
+
+        
+        {
+            id: 11,
+            title: "Bee a Sweet Home",
+            category: "Ambiente",
+            img: "https://images.unsplash.com/photo-1547637886-84deffdf4c17?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+            description: "Participa en la construcción y colocación de Hoteles para abejas en los parques urbanos de San José.",
+            organization : "AbejasNativas CR",
+            date: "Octubre 12",
+            duration: "1"
+    
+        },
+
+        {
+            id: 12,
+            title: "Alacenas Llenas",
+            category: "Salud y bienestar",
+            img: "https://images.unsplash.com/photo-1547637886-84deffdf4c17?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+            description: "Jornada de recolección y empaque de víveres para las familias afectadas por el Huracán Hannah.",
+            organization : "Bomberos de Costa Rica",
+            date: "Septiembre 18",
+            duration: "3 días"
+    
+        },
+    
+    ];
+
+    export default DataCampaign;
+
+
+
