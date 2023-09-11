ce3189df941ed77eb475fce5bf0827d4d85ac8cc
Adriela López <110007997+Adriela23@users.noreply.github.com>
2023-09-11 21:10:44 +0000

    rama creada y archivos iniciados

---
+++ src/front/js/pages/orgAgregarEvento.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {navbar} from "..component/navbar";
+import { formOrgAgregarEvento } from "../component/formOrgAgregarEvento";
+
+export const orgAgregarEvento = () => {
+    return(
+        <div>
+            <formOrgAgregarEvento/>
+        </div>
+    );
+}
