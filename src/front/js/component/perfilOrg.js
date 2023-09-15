05eac40f9e1d2e687d3547db33eafe6eddf5ae02
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-09-15 21:56:29 +0000

    updated perfilOrg layout

--- src/front/js/component/perfilOrg.js
+++ src/front/js/component/perfilOrg.js
@@ -9,30 +9,23 @@ const perfilOrg = () => {
                 <div>
                     <h1>Mi Perfil</h1>
                 </div>
-                <div className='mt-5'>
-                    <h5>Mis eventos</h5>
+
+                <div className='row mt-5'>
+                    <div className='col-6'>
+                        <h4>Mis eventos</h4>
+                        <p>En esta sección puedes añadir nuevos eventos que serán publicados en la plataforma ETHOS. Así nuestros voluntarios podrán agendar tus campañas en su Google Calendar.</p>
+                        <p>Si necesitas asistencia, <Link to={"/Contacto"}>contáctanos</Link>.</p>
+                    </div>
+
+                    <div className='col'>
+                                <button type="button" className="add-event btn btn-danger">Crear envento</button>
+                    </div>
                 </div>
-                <table className="table table-dark">
-                    <thead>
-                        ...
-                    </thead>
-                    <tbody>
-                        <tr className="table-active">
-                            ...
-                        </tr>
-                        <tr>
-                            ...
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td colspan="2" className="table-active">Larry the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </table>
-            <Link to={"/"} style={{ textDecoration: 'none' }}>
-                <i className='fas fa-arrow-alt-circle-left'></i>
-            </Link>
+                
+                <Link to={"/"} style={{ textDecoration: 'none' }}>
+                    <i className='fas fa-arrow-alt-circle-left'></i>
+                </Link>
+                
             </div>
         </>
     )
